3c33204da98e13f6fd5ff5201eb99e9f38612d3c
Allan Reis <allanreis3006@gmail.com>
2024-05-22 21:10:44 -0300

    feat: utilização da API

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import errorHandler from './middleware/error.js'
 import logger from './middleware/logger.js'
 import notFound from './middleware/notFound.js'
 import posts from './routes/posts.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+// Get the directory
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = new express()
 
@@ -10,6 +16,9 @@ const app = new express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(logger)
+
+// Setup static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/api/posts', posts)
@@ -23,7 +32,3 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
-
-// Setup static folder
-// const path = require('path')
-// app.use(express.static(path.join(__dirname, 'public')))
